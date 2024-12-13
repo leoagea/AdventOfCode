001727bf25af16c78e00c24160839fee46a1b1f8
Leo AGEA <leoagea@gmail.com>
2024-12-13 17:08:42 +0100

    Update : day7 part1 done

---
+++ 2024/day7/part1/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+bool solveLine(long val, vector<long> &vec, long current, int index){
+    if(index == vec.size())
+        return val == current;
+    return solveLine(val, vec, current + vec[index], index + 1) \
+        || solveLine(val, vec, current * vec[index], index + 1);
+}
+
+
+int main(){
+    ifstream file("../data.txt");
+    string line;
+    unsigned long long res = 0;
+
+    while(getline(file, line)){
+        stringstream ss(line);
+        string key;
+        getline(ss, key, ':');
+        cout << key << endl;
+        long val = stol(key);
+        getline(ss, key, ' ');
+        vector<long> vec;
+        while(getline(ss , key , ' ')){
+            cout << key << " ";
+            vec.push_back(stol(key));
+        }
+        cout << endl;
+        if(solveLine(val, vec, vec[0], 1))
+            res += val;
+    }
+    cout << res << endl;
+}
